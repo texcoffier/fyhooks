722cd4268c2f9fe2cf6036f2718577deadf51a00
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2021-12-05 20:30:11 +0100

    Add decorator

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ import reactor
 R = reactor.Reactor()
 class Model:
     variables = {}
+    history = []
+    def __str__(self):
+        t = []
+        t.append(f'\n\tmodel={M.variables}')
+        t.append(f'\n\thistory')
+        for history in M.history:
+            t.append(f'\n\t\t{history}')
+        return ''.join(t)
 M = Model()
 
-
+@R.handler('START')
 def _start(event:reactor.Event):
     class StdinReader(threading.Thread):
         def run(self):
@@ -21,24 +29,25 @@ def _start(event:reactor.Event):
         def run(self):
             while True:
                 R('timer')
-                time.sleep(1)
+                time.sleep(5)
     R('print', 'start threads')
     Timer().start()
     StdinReader().start()
-R.add('START', _start, 'A')
 
+@R.handler('print')
 def _print(event:reactor.Event):
     print('print', event.data[1])
-R.add('print', _print, 'B')
 
+@R.handler('dump')
 def _dump(event:reactor.Event):
-    R('print', f"event={event} model={M.variables}")
-R.add('dump', _dump, 'D')
+    R('print', str(M))
+    M.history.pop()
 
+@R.handler('timer')
 def _timer(event:reactor.Event):
     M.variables['T'] = M.variables.get('T', 0) + 1
-R.add('timer', _timer, 'T')
 
+@R.handler('stdin')
 def _calc(event:reactor.Event):
     try:
         result = eval(event.data[1], {}, M.variables)
@@ -46,8 +55,8 @@ def _calc(event:reactor.Event):
         return True
     except:
         pass
-R.add('stdin', _calc, 'C2')
 
+@R.handler('stdin')
 def _set(event:reactor.Event):
     try:
         var, val = event.data[1].split('=', 1)
@@ -56,13 +65,16 @@ def _set(event:reactor.Event):
         return True
     except:
         pass
-R.add('stdin', _set, 'C')
 
+@R.handler('stdin')
 def _do_dump(event:reactor.Event):
     if event.data[1] == 'p\n':
         R('dump')
         return True
-R.add('stdin', _do_dump, 'A')
+
+@R.handler('', 'A')
+def _record(event:reactor.Event):
+    M.history.append(str(event.data))
 
 
 R('START')
--- reactor.py
+++ reactor.py
@@ -7,18 +7,31 @@ Priority = str
 class Reactor:
     def __init__(self):
         self.handlers:Dict[EventType,List[Tuple[Priority, Handler]]] = {}
+        self.sorted_handlers:Dict[EventType, List[Handler]] = {}
 
     def add(self, event_type:EventType, handler:Handler, priority:Priority):
         if event_type not in self.handlers:
             self.handlers[event_type] = []
-        self.handlers[event_type].append((priority, handler))
-        self.handlers[event_type].sort()
+        handlers = self.handlers[event_type]
+        handlers.append((priority, len(handlers), handler))
+        handlers.sort()
+        self.sorted_handlers[event_type] = [handler[2] for handler in handlers]
 
     def __call__(self, *data):
         event = Event(data)
-        for _, handler in self.handlers.get(data[0], []):
+        for handler in self.sorted_handlers.get(data[0], []):
             if handler(event):
                 break
+
+    def handler(self, event_type:EventType, priority:Priority = 'MMMMM'):
+        def handler(function):
+            if event_type:
+                self.add(event_type, function, priority)
+            else:
+                for event in self.handlers:
+                    self.add(event, function, priority)
+            return function
+        return handler
 
 class Event:
     def __init__(self, data):
